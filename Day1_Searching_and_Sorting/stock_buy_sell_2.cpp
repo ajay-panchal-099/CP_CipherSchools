c7ef4a66696f29eb528c6d125fd00c46d45c6d44
AJAY PANCHAL <akpanchal099@gmail.com>
2021-02-14 22:09:15 +0530

    one transactions

---
+++ Day1_Searching_and_Sorting/stock_buy_sell_2.cpp
@@ -0,0 +1,47 @@
+/*
+ *
+ * Author :  Ajay Panchal
+ * B.TECH  CSE STUDENT
+ * LOVELY PROFESSIONAL UNIVERSITY
+ * BATCH : 2018-2022
+ * GITHUB LINK : https://github.com/ajay-panchal-099/CP_CipherSchools
+ * PROBLEM STATEMENT : stock buy and sell (one  transactions)
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int                            long long
+#define INF                            0x3f3f3f3f
+
+#define fast_io                        ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define IO_Problem                 freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+
+void Profit(vector<int>& vec){
+
+    int profit = 0;
+    int mn = vec[0];
+
+    for (int i = 1; i <  vec.size(); ++i) {
+       if(vec[i] < mn)
+           mn = vec[i];
+       profit = max(profit, vec[i] - mn);
+    }
+
+    cout<<profit<<endl;
+
+}
+
+
+int32_t main()
+{
+    fast_io;
+    // IO_Problem;
+
+    vector<int>vec{ 100, 180, 260, 310,
+                    40, 535, 695};
+
+    Profit(vec);
+
+    return 0;
+}
