482530eb86f25470d95a4b9c75b50935e1b343da
AJAY PANCHAL <akpanchal099@gmail.com>
2021-02-16 13:23:29 +0530

    Add files via upload

---
+++ Day2_Recursion/Excel_sheet_column_title.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string convertToTitle(int n) {
+        
+       string s="";
+        n--;
+     while(n>=0){
+        s+=('A'+n%26);
+        n/=26;
+        n--;
+        }
+     reverse(s.begin(),s.end());
+     return s;
+    }
+};
