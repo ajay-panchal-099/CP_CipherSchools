4b560468033df5beabac7c19d967992513a437b0
AJAY PANCHAL <akpanchal099@gmail.com>
2021-02-21 00:11:14 +0530

    Add files via upload

---
+++ BST_HEAP_HASHING/Kth_Smallest_BST.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+   vector<int>ans;
+    
+    void inorder(TreeNode* root){ 
+          
+        if(root==NULL)
+            return;
+        
+        
+        inorder(root->left);
+        ans.push_back(root->val);
+        inorder(root->right);
+    }
+    
+    
+    int kthSmallest(TreeNode* root, int k) {
+        
+      inorder(root);
+        
+       // for( auto it : ans) cout<<it<<" ";
+        return ans[k-1];
+        
+    }
+};
